6a5cbe1348e93470d3697f1e35392bb56b93b5b2
aimaar16 <122547017+aimaar16@users.noreply.github.com>
2024-11-24 21:14:37 +0100

    Update logout.php

--- app/logout.php
+++ app/logout.php
@@ -1,5 +1,13 @@
 <?php
-session_start(); //Inicia la sesión
+
+
+session_set_cookie_params([
+    'httponly' => true,
+    'secure' => isset($_SERVER['HTTPS']), // Asegura que la cookie sea solo para HTTPS
+    'samesite' => 'Strict',              // Opcional: protege contra CSRF
+]);
+session_start(); // Iniciar sesió
+session_regenerate_id(true);
 session_destroy(); // Destruye la sesión
 header("Location: index.php"); // Redirige a la página principal
 exit();
