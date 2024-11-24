6753954b33f6d33dbe49a011f914090a248fdd26
aimaar16 <122547017+aimaar16@users.noreply.github.com>
2024-11-24 21:18:00 +0100

    Update show_item.php

--- app/show_item.php
+++ app/show_item.php
@@ -1,6 +1,12 @@
 <?php
-// Iniciar sesión
-session_start();
+
+session_set_cookie_params([
+    'httponly' => true,
+    'secure' => isset($_SERVER['HTTPS']), // Asegura que la cookie sea solo para HTTPS
+    'samesite' => 'Strict',              // Opcional: protege contra CSRF
+]);
+session_start(); // Iniciar sesió
+session_regenerate_id(true);
 
 // Verificar si el usuario está logueado
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
